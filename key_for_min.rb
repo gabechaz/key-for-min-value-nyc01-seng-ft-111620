361e4800b736eda3bae60448c6d94e6e44659add
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-10-21 23:07:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,12 @@
 
 def key_for_min_value(name_hash)
 
+min_value_key = nil
+min_value = 100
+name_hash.each do |key, value|
+  if value < min_value
+    min_value = value
+    min_value_key = key
+  end
+end
 end
